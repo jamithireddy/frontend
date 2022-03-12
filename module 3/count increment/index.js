ffcbc27c6c1532459edef45efbf81debae0c557c
Rajasekhar Jamithireddy <jamithireddy@outlook.com>
2022-03-12 17:40:21 +0530

    counter app

--- module 3/count increment/index.js
+++ module 3/count increment/index.js
@@ -1,6 +1,15 @@
-let countEl=document.getElementById("count-el")
+let countEl = document.getElementById("count-el")
+let finalCnt = document.getElementById("final-cnt")
 let count =0
+
 function incrmnt(){
-  count+=1
+  count += 1
   countEl.innerText=count
+}
+
+function save(){
+  let counter = count + " - "
+  count = 0
+  finalCnt.textContent += counter
+  countEl.textContent = 0
 }
