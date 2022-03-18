295bef3bc65c6679ed781da80c51112ad8acfabe
Rajasekhar Jamithireddy <jamithireddy@outlook.com>
2022-03-18 09:51:04 +0530

    corrected JS

--- module 3/password_generator/my_solution/index.js
+++ module 3/password_generator/my_solution/index.js
@@ -1,33 +1,38 @@
-/* Still have to solve the user input problem */
-let lenEl = document.getElementById("length-el");
-const len=lenEl.value;
-let title =document.getElementById("title-el")
-let arr=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","W","X","Y","Z",0,1,2,3,4,5,6,7,8,9,"@","#","$","%","^","&","*","(",")","!"]
-let firstEl = document.getElementById("first-el")
-let secondEl = document.getElementById("second-el")
-let thirdEl = document.getElementById("third-el")
-let fourthEl = document.getElementById("fourth-el")
-let pass=""
+const titleEl=document.getElementById("title-el")
+const lenEl=document.getElementById("pwd-len")
+const generatePswd =document.getElementById("generate-pwd")
+const firstEl=document.getElementById("first-el")
+const secondEl=document.getElementById("second-el")
+const thirdEl=document.getElementById("third-el")
+const fourthEl=document.getElementById("fourth-el")
+let pw=[]
+arr=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",1,2,3,4,5,6,7,8,9,0,"!","@","#","$","%","^","&","*","(",")"]
 
-function pwdGen(){
-    let pass=""
-    if (Number.isInteger(len) && (len>=1 && len<=13)){
-        for (let i=0;i<len;i++){
-            pass += arr[getRandom()]  
+generatePswd.addEventListener("click",genPswd)
+function randNum(){
+    return Math.floor(Math.random()*71)+1
+}
+ 
+function genPswd(){
+    if (Number.isInteger(parseInt(lenEl.value)) && (parseInt(lenEl.value) >=1 && parseInt(lenEl.value)<=13)){
+        for (let i=0;i<4;i++){
+            let pass=""
+            for(let j=0;j<parseInt(lenEl.value);j++){
+                pass+=arr[randNum()]
+            }
+            pw.push(pass)
         }
-        return pass 
+    firstEl.textContent=pw[0]
+    secondEl.textContent=pw[1]
+    thirdEl.textContent=pw[2]
+    fourthEl.textContent=pw[3]  
     }else{
-        title.textContent="Check your Input. Input needs to be an integer between 1 and 12"
-        title.style.color="red";
+        titleEl.textContent="Enter a Integer value between 1 and 13"
+        titleEl.style.color="red";
     }
 }
-function getRandom(){
-    return Math.floor(Math.random()*70)+1
-}
 
-function generatePswd(){
-    firstEl.textContent=pwdGen()
-    secondEl.textContent=pwdGen()
-    thirdEl.textContent=pwdGen()
-    fourthEl.textContent=pwdGen()
-}
+
+
+
+
